f1c9bc4a0208c108e2c96dbe1052d3b5884f2de9
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-09-18 09:53:45 -0500

    Cleaning up and added a transition to closed.

--- jiraRelease.rb
+++ jiraRelease.rb
@@ -33,58 +33,128 @@ printVars({:project=>project,
 		   :version=>version,
 		   :jiraURLBase=>jiraURLBase})
 
-username, password = userpass.split(':')
 # TODO If password is empty, ask for it.
+if userpass.include? ':'
+	@username, @password = userpass.split(':')
+else
+	@username = userpass
+	#password = 
 
-rest2 = URI(jiraURLBase + '/rest/api/2/')
+	# if darwin
+	
+	@password = `security 2>&1 >/dev/null find-internet-password -gs "#{jiraURLBase}" -a "#{@username}"`
+    @password[/^password: "(.*)"$/, 1]
+end
 
-Net::HTTP.start(rest2.host, rest2.port, :use_ssl=>true) do |http|
+@rest2 = URI(jiraURLBase + '/rest/api/2/')
+
+Net::HTTP.start(@rest2.host, @rest2.port, :use_ssl=>true) do |http|
 	### Create new version
-#	request = Net::HTTP::Post.new(rest2 + 'version')
-#	request.content_type = 'application/json'
-#	request.basic_auth(username, password)
-#	request.body = JSON.generate({
-#		'name' => version,
-#		'archived' => false,
-#		'released' => true,
-#		'releaseDate' => DateTime.now.strftime('%Y-%m-%d'),
-#		'project' => project,
-#	})
-#
-#	response = http.request(request)
-
-
-	### Find all unreleased issues
-	query ="project = #{project} AND (status = Resolved OR status = Closed) AND fixVersion = EMPTY" 
-	#query ="project = #{project} AND (status = Resolved OR status = Closed)" 
-	request = Net::HTTP::Post.new(rest2 + 'search')
-	#puts request.uri
+	request = Net::HTTP::Post.new(@rest2 + 'version')
 	request.content_type = 'application/json'
-	request.basic_auth(username, password)
+	request.basic_auth(@username, @password)
 	request.body = JSON.generate({
-		'jql' => query,
-		'fields' => [ "key" ]
+		'name' => version,
+		'archived' => false,
+		'released' => true,
+		'releaseDate' => DateTime.now.strftime('%Y-%m-%d'),
+		'project' => project,
 	})
 
 	response = http.request(request)
 	case response
 	when Net::HTTPSuccess
-		issues = JSON.parse(response.body)
-		keys = issues['issues'].map {|item| item['key'] }
-		puts keys
+
+	else
+		puts "failed because"
+		exit 1
 	end
 
-	### Mark issues as fixed by version
-#	update = JSON.generate({ "update" => { "fixVersions"=>[{"add"=>{"name"=>version}}]} })
-#	keys.each do |key|
-#		request = Net::HTTP::Put.new(rest2 + ('issue/' + key))
-#		request.content_type = 'application/json'
-#		request.basic_auth(username, password)
-#		request.body = update
-#
-#		response = http.request(request)
-#	end
+	def getIssueKeys(http, query)
+		request = Net::HTTP::Post.new(@rest2 + 'search')
+		request.content_type = 'application/json'
+		request.basic_auth(@username, @password)
+		request.body = JSON.generate({
+			'jql' => query,
+			'fields' => [ "key" ]
+		})
 
+		response = http.request(request)
+		case response
+		when Net::HTTPSuccess
+			issues = JSON.parse(response.body)
+			keys = issues['issues'].map {|item| item['key'] }
+			return keys
+		else
+			return []
+		end
+	end
+
+	### Find all unreleased issues
+	query ="project = #{project} AND (status = Resolved OR status = Closed) AND fixVersion = EMPTY" 
+	keys = getIssueKeys(http, query)
+	printVars({:keys=>keys})
+
+	### Mark issues as fixed by version
+	update = JSON.generate({ 'update' => { 'fixVersions'=>[{'add'=>{'name'=>version}}]} })
+	keys.each do |key|
+		request = Net::HTTP::Put.new(@rest2 + ('issue/' + key))
+		request.content_type = 'application/json'
+		request.basic_auth(@username, @password)
+		request.body = update
+
+		response = http.request(request)
+		case response
+		when Net::HTTPSuccess
+		else
+			puts "failed on #{key} because #{response}"
+		end
+	end
+
+	### Transition to Closed
+	# We don't currently use the difference between Resolved and Closed.  So make all Closed.
+	query = "project = #{project} AND status = Resolved AND fixVersion != EMPTY" 
+	keys = getIssueKeys(http, query)
+	printVars({:Rkeys=>keys})
+
+	if !keys.empty?
+
+		# *sigh* Need to transition by ID, buts what's the ID? So look that up
+		request = Net::HTTP::Get.new(@rest2 + ('issue/' + keys.first + '/transitions'))
+		request.content_type = 'application/json'
+		request.basic_auth(@username, @password)
+		response = http.request(request)
+		case response
+		when Net::HTTPSuccess
+			trans = JSON.parse(response.body)
+			closed = trans['transitions'].select {|item| item['name'] == 'Closed' }
+		else
+			puts "failed because"
+			exit 1
+		end
+
+		if closed.empty?
+			puts "Cannot find Transition to Closed!!!!"
+			exit 2
+		end
+
+		printVars({:closedID=>closed[0]['id']})
+		
+		update = JSON.generate({'transition'=>{'id'=> closed[0]['id'] }})
+		keys.each do |key|
+			request = Net::HTTP::Post.new(@rest2 + ('issue/' + key + '/transitions'))
+			request.content_type = 'application/json'
+			request.basic_auth(@username, @password)
+			request.body = update
+
+			response = http.request(request)
+			case response
+			when Net::HTTPSuccess
+			else
+				puts "failed on #{key} because #{response}"
+			end
+		end
+	end
 end
 
 #  vim: set sw=4 ts=4 :
